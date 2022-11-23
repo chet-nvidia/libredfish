2fbd06a26abd1bb4cef1b36dc8e4739d2efac8c1
Matthias Einwag <meinwag@nvidia.com>
2022-11-23 19:12:49 +0000

    cargo fmt and make clippy happy
    This fixes the rustfmt deviations and clippy lints.

--- src/bios.rs
+++ src/bios.rs
@@ -1,6 +1,6 @@
+use crate::common::*;
 use std::fmt;
 use std::fmt::Formatter;
-use crate::common::*;
 
 #[derive(Debug, Deserialize, Clone)]
 #[serde(rename_all = "PascalCase")]
@@ -185,7 +185,7 @@ pub struct OemDellBiosAttributes {
 #[serde(rename_all = "PascalCase")]
 pub struct OemDellSoftwareImage {
     pub software_images: Vec<ODataId>,
-    pub active_software_image: ODataId
+    pub active_software_image: ODataId,
 }
 
 #[derive(Debug, Deserialize, Clone)]
@@ -208,7 +208,7 @@ pub enum RedfishSettingsApplyTime {
     AtMaintenanceWindowStart,
     Immediate,
     InMaintenanceWindowOnReset,
-    OnReset,    // preferred
+    OnReset, // preferred
 }
 
 impl fmt::Display for RedfishSettingsApplyTime {
@@ -225,7 +225,7 @@ pub struct SetOemDellBiosSettingsApplyTime {
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub enum SerialCommSettings {
-    OnConRedir,     // preferred
+    OnConRedir, // preferred
     OnNoConRedir,
     Off,
 }
@@ -238,7 +238,7 @@ impl fmt::Display for SerialCommSettings {
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub enum SerialPortSettings {
-    Com1,     // preferred
+    Com1, // preferred
     Com2,
 }
 
@@ -250,7 +250,7 @@ impl fmt::Display for SerialPortSettings {
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub enum SerialPortExtSettings {
-    Serial1,     // preferred
+    Serial1, // preferred
     Serial2,
     RemoteAccDevice,
 }
@@ -263,7 +263,7 @@ impl fmt::Display for SerialPortExtSettings {
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub enum SerialPortBaudSettings {
-    Speed115200bps = 115200,     // preferred
+    Speed115200bps = 115200, // preferred
     Speed57600bps = 57600,
     Speed19200bps = 19200,
     Speed9600bps = 9600,
@@ -277,7 +277,7 @@ impl fmt::Display for SerialPortBaudSettings {
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub enum SerialPortTermSettings {
-    Vt100Vt220,     // preferred
+    Vt100Vt220, // preferred
     Ansi,
 }
 
@@ -355,7 +355,7 @@ pub struct OemDellBiosTpmAttrs {
 pub struct SetOemDellBiosTpmAttrs {
     #[serde(rename = "@Redfish.SettingsApplyTime")]
     pub redfish_settings_apply_time: SetOemDellBiosSettingsApplyTime,
-    pub attributes: OemDellBiosTpmAttrs
+    pub attributes: OemDellBiosTpmAttrs,
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
@@ -369,7 +369,6 @@ impl fmt::Display for UefiVariableAccessSettings {
         fmt::Debug::fmt(self, f)
     }
 }
-
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 #[serde(rename_all = "PascalCase")]
@@ -389,6 +388,6 @@ pub struct SetOemDellBiosLockdownAttrs {
 #[test]
 fn test_bios_parser() {
     let test_data = include_str!("../tests/bios.json");
-    let result: OemDellBios = serde_json::from_str(&test_data).unwrap();
+    let result: OemDellBios = serde_json::from_str(test_data).unwrap();
     println!("result: {:#?}", result);
 }
