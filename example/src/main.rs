564bd1d89e8fd4a07586e354da2f7aea032307cd
Vishnu Rangayyan <vrangayyan@nvidia.com>
2022-12-06 16:09:02 -0600

    Add setting up serial console access

--- example/src/main.rs
+++ example/src/main.rs
@@ -49,67 +49,72 @@ fn main() -> Result<(), reqwest::Error> {
                 redfish.set_system_power(libredfish::system::SystemPowerControl::ForceRestart)?;
             }
             "shutdown" => {
-                redfish.set_system_power(libredfish::system::SystemPowerControl::GracefulShutdown)?;
+                redfish
+                    .set_system_power(libredfish::system::SystemPowerControl::GracefulShutdown)?;
             }
             "restart" => {
-                redfish.set_system_power(libredfish::system::SystemPowerControl::GracefulRestart)?;
+                redfish
+                    .set_system_power(libredfish::system::SystemPowerControl::GracefulRestart)?;
             }
-            "status" => {
-                match redfish.get_system() {
-                    Ok(system) => {
-                        println!("System power status: {}", system.power_state);
-                    }
-                    Err(e) => {
-                        eprintln!("Error: {}", e);
-                    }
+            "status" => match redfish.get_system() {
+                Ok(system) => {
+                    println!("System power status: {}", system.power_state);
                 }
-            }
+                Err(e) => {
+                    eprintln!("Error: {}", e);
+                }
+            },
             "tpm_enable" => {
                 redfish.enable_tpm()?;
+                println!("BIOS settings changes require system restart");
             }
             "tpm_disable" => {
                 redfish.disable_tpm()?;
+                println!("BIOS settings changes require system restart");
             }
             "tpm_reset" => {
                 redfish.reset_tpm()?;
+                println!("BIOS settings changes require system restart");
             }
             "serial_enable" => {
+                redfish.setup_bmc_remote_access()?;
                 redfish.setup_serial_console()?;
+                println!("BIOS settings changes require system restart");
             }
             "lockdown_enable" => {
                 redfish.enable_bios_lockdown()?;
-                return redfish.enable_bmc_lockdown();
+                redfish.enable_bmc_lockdown()?;
+                println!("BIOS settings changes require system restart");
             }
             "lockdown_disable" => {
                 redfish.disable_bmc_lockdown()?;
-                return redfish.disable_bios_lockdown();
+                redfish.disable_bios_lockdown()?;
+                println!("BIOS settings changes require system restart");
             }
-            "bios_attrs" => {
-                match redfish.get_bios_data() {
-                    Ok(bios) => {
-                        println!("{:?}", bios);
-                    }
-                    Err(e) => {
-                        eprintln!("Error: {}", e);
-                    }
+            "bios_attrs" => match redfish.get_bios_data() {
+                Ok(bios) => {
+                    println!("{:?}", bios);
                 }
-            }
-            "bmc_attrs" => {
-                match redfish.get_bmc_data() {
-                    Ok(bmc) => {
-                        println!{"{:?}", bmc};
-                    }
-                    Err(e) => {
-                        eprintln!("Error: {}", e);
-                    }
+                Err(e) => {
+                    eprintln!("Error: {}", e);
                 }
-            }
+            },
+            "bmc_attrs" => match redfish.get_bmc_data() {
+                Ok(bmc) => {
+                    println! {"{:?}", bmc};
+                }
+                Err(e) => {
+                    eprintln!("Error: {}", e);
+                }
+            },
             _ => {
-                eprintln!("Unsupported command specified {}", args_given.opt_str("c").unwrap());
+                eprintln!(
+                    "Unsupported command specified {}",
+                    args_given.opt_str("c").unwrap()
+                );
             }
         }
     }
 
     Ok(())
 }
-
